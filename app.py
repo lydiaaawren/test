98712169c539441fc57960a77038f4a129a20f8a
lydiaaawren <165889010+lydiaaawren@users.noreply.github.com>
2024-11-20 14:19:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ st.write(
     """
 )
 
+# Enter name
+
+name_on_order = st.text_input("Name on smoothie:")
+#st.write("The name on your smoothie is", name_on_order)
+
 # Fruit table
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -18,6 +23,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
     , my_dataframe
+    , max_selections = 5
 )
 
 if ingredients_list:
@@ -27,10 +33,11 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
     #st.write(ingredients_string)
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-    values ( ' """ + ingredients_string + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+    values ( ' """ + ingredients_string + """','"""+name_on_order+ """')"""
     #st.write(my_insert_stmt)
+
     time_to_insert = st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
-        st.success('Your smoothie is ordered!')
+        st.success('Your smoothie is ordered, '+name_on_order+'!')
